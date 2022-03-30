019255f52451db6f20072622d7e10240bff74313
Daniil Zhmak <daniil_zhmak@epam.com>
2022-03-30 10:33:49 +0000

    | EM-63 | Feature | Add activeadmin |

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module EpamMusic
   class Application < Rails::Application
     config.load_defaults 7.0
     config.api_only = true
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: Rails.application.credentials.cookie_store_key
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admins, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   namespace :api do
     namespace :v1 do
       resource :session, only: %i[create destroy update]

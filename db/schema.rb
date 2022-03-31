bcb501184c8007d6cd8a5a8f5021bdc83658b10b
Daniil Zhmak <daniil_zhmak@epam.com>
2022-03-31 18:39:15 +0000

    | EM-89 | Bug | Fix strong_migrations issues |

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_30_113014) do
   create_table "admins", force: :cascade do |t|
     t.string "email", null: false
     t.string "encrypted_password", null: false
-    
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true

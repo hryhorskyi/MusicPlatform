20e2b6301f9c1eeb6bff12ed631a51e2483bdeff
Martin Hryhorskyi <martin_hryhorskyi@epam.com>
2022-03-31 12:09:52 +0000

    | EM-66 | Feature | Add entity artists |

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_28_163943) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_30_113014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_28_163943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_artists_on_name", unique: true
   end
 
   create_table "friends", force: :cascade do |t|

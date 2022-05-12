f868020a5aab767f176f57a440059afd8da50a36
Dmytro Lukash <dmytro_lukash@epam.com>
2022-05-12 16:48:22 +0000

    | EM-105 | Feature | Setup docker

--- config/initializers/jwt_session.rb
+++ config/initializers/jwt_session.rb
@@ -3,6 +3,7 @@
 JWTSessions.algorithm = 'HS256'
 JWTSessions.encryption_key = Rails.application.credentials.secret_key_base
 JWTSessions.token_store =  :redis, {
-  redis_url: ENV.fetch('REDIS_URL', Rails.application.credentials.dig(:redis, :url)),
+  redis_url: ENV.fetch('DOCKER_REDIS_URL',
+                       nil) || ENV.fetch('REDIS_URL', Rails.application.credentials.dig(:redis, :url)),
   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
 }

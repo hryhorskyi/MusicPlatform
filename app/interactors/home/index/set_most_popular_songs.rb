6048c8a1b7d1ec2ebc1a852218552408b81b481f
Mykhailo Rohovyk <mykhailo_rohovyk@epam.com>
2022-06-16 20:56:41 +0000

    | EM-34 | Feature | Add most popular songs to home index |

--- app/interactors/home/index/set_most_popular_songs.rb
+++ app/interactors/home/index/set_most_popular_songs.rb
@@ -12,7 +12,13 @@ module Home
       private
 
       def find_songs
-        Song.last(SONGS_QUANTITY)
+        cached_song_ids = Rails.cache.fetch('popular_songs', expires_in: 5.minutes) { most_popular_song_ids }
+        Song.where(id: cached_song_ids)
+      end
+
+      def most_popular_song_ids
+        popular_songs = PlaylistSong.group('playlist_songs.song_id').order('count(playlist_songs.song_id) desc').count
+        popular_songs.keys.first(SONGS_QUANTITY)
       end
     end
   end
--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class SongSerializer < BaseSerializer
+  belongs_to :album
   attributes :name, :featured
 end

78194c9e712f3fbc4939f5ca2a0471eb71c381fa
Martin Hryhorskyi <martin_hryhorskyi@epam.com>
2022-06-21 11:27:53 +0000

    | EM-183 | Feature | Add latest songs to home index |

--- spec/interactors/home/index/set_latest_songs_spec.rb
+++ spec/interactors/home/index/set_latest_songs_spec.rb
@@ -4,16 +4,30 @@ RSpec.describe Home::Index::SetLatestSongs do
   describe '.call' do
     subject(:result) { described_class.call(model: model) }
 
+    before do
+      create_list(:song, 10)
+      create_list(:playlist_song, 10)
+    end
+
     let(:model) { Struct.new(:latest_songs).new }
 
-    context 'with precreated songs' do
-      before do
-        create_list(:song, 10)
+    context 'when correct' do
+      let!(:latest_song) { create(:song) }
+
+      it 'has included latest song in top list' do
+        expect(result.model.latest_songs).to include(latest_song)
       end
 
       it 'set right quantity of songs' do
         expect(result.model.latest_songs.count).to eq(described_class::SONGS_QUANTITY)
-        expect(result).to be_success
+      end
+    end
+
+    context 'when incorrect' do
+      let(:latest_song) { nil }
+
+      it 'has top list without latest song' do
+        expect(result.model.latest_songs).not_to include(latest_song)
       end
     end
   end
